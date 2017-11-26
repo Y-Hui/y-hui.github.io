be5677e83fe170d89e4320137309931596f95554
Y-Hui <1829804817@qq.com>
2017-11-26 15:31:16 +0800

    Site updated: 2017-11-26 15:31:15

--- js/common.js
+++ js/common.js
@@ -10,32 +10,41 @@ $(function () {
         oMask.removeClass("overlay-open");
     });
 
-});
-
-$(function () {
     // 项目界面选项（project item）
     var oItem = $(".project li:first");
     oItem.addClass("active");
-});
 
-$(function () {
     // nav active
     var oTitle = $(".banner-title").html();
-    if(oTitle == "Project"){
+    if (oTitle == "Project") {
         var oNav = $(".nav-item li:eq(1)");
         oNav.addClass("active");
     }
-    else if (oTitle == "Design"){
+    else if (oTitle == "Design") {
         var oNav = $(".nav-item li:eq(2)");
         oNav.addClass("active");
     }
-    else if (oTitle == "About"){
+    else if (oTitle == "About") {
         var oNav = $(".nav-item li:eq(3)");
         oNav.addClass("active");
     }
-    else{
+    else {
         var oNav = $(".nav-item li:first");
         oNav.addClass("active");
     }
-});
 
+    // top btn
+    var top = $('.scroll-top');
+    top.on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 600)
+    })
+
+    document.addEventListener('scroll', function () {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+        if (scrollTop > 100) {
+            top.addClass('opacity');
+        } else {
+            top.removeClass('opacity');
+        }
+    });
+});
